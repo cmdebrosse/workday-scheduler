98d66861fc476eb54649ef29f1e2ba66933bd879
Charles DeBrosse <cmdebrosse@outlook.com?>
2021-12-02 20:02:45 -0500

    finished logic

--- script.js
+++ script.js
@@ -1,2 +1,51 @@
+// Display present date and time 
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 $("#localTime").text(moment().format("LT"));
+
+
+$(document).ready(function(){
+    // Save time and text from class ".description" to local storage
+    $(".saveBtn").on("click", function() {
+        var descriptionText = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, descriptionText);
+    });
+});
+
+// Get saved values from local storage for each time block
+$("#7 .description").val(localStorage.getItem("7"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
+$("#21 .description").val(localStorage.getItem("21"));
+$("#22 .description").val(localStorage.getItem("22"));
+
+// Change background color of timeblocks based on the time of day
+function timeLine() {
+    var currentHour = moment().hour();
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id"));
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+        } else if (blockHour === currentHour) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+        } else {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        };
+    });
+};
+
+timeLine();
